b47757b24c01b2bdcb2cdcacdd40b052607b5a20
David Przybilla <dav.alejandro@gmail.com>
2022-12-02 23:58:52 +0900

    updating modules

--- flake.nix
+++ flake.nix
@@ -1,13 +1,35 @@
 {
-  description = "A very basic flake";
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+  inputs.theme-module.url = path:./themes/minima;
+  outputs = { self, nixpkgs, theme-module}:#, #theme-module}:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in{
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
-
-  
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in rec {
+          blog = pkgs.stdenv.mkDerivation {
+          name = "dav009_blog"; # our package name, irrelevant in this case
+          src = ./.;
+            buildPhase = ''
+              ${pkgs.hugo}/bin/hugo --minify
+            '';
+            installPhase = ''
+              cp -r public $out
+            '';
+          };
+          default = blog;
+        });  
+      
+     devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ hugo ];
+          };
+      });
+    };
 }
